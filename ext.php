89c453714d5846d9a53fa9b7656aab270ba41b09
cypherbits <info@avanix.es>
2020-10-30 11:30:05 +0100

    Require phpBB >= 3.3.1

--- ext.php
+++ ext.php
@@ -18,4 +18,9 @@ namespace cypherbits\onionbb;
  */
 class ext extends \phpbb\extension\base
 {
+    public function is_enableable()
+    {
+        $config = $this->container->get('config');
+        return phpbb_version_compare($config['version'], '3.3.1', '>=');
+    }
 }
